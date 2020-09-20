60c26d48948ca11b3cdca6c048c7943d7da2f8aa
Melissa LeBlanc-Williams <melissa@adafruit.com>
2020-09-19 20:42:00 -0600

    Reformatted to latest version of black

--- docs/conf.py
+++ docs/conf.py
@@ -147,7 +147,13 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, "opensignlibrary", "OpenSign Library Documentation", [author], 1,),
+    (
+        master_doc,
+        "opensignlibrary",
+        "OpenSign Library Documentation",
+        [author],
+        1,
+    ),
 ]
 
 # -- Options for Texinfo output -------------------------------------------
